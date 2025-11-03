7b220ab4986d4ca30244cb8c340c08eb80ddee41
ajaypangare <pangare.ajay@jamunjar.com>
2025-11-03 11:36:06 +0530

    Refactor SmsOtpAutoVerifyPlugin for clarity and efficiency

--- android/src/main/kotlin/com/oohyugi/sms_otp_auto_verify/SmsOtpAutoVerifyPlugin.kt
+++ android/src/main/kotlin/com/oohyugi/sms_otp_auto_verify/SmsOtpAutoVerifyPlugin.kt
@@ -1,34 +1,30 @@
 package com.oohyugi.sms_otp_auto_verify
 
 import android.app.Activity
-import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-
 import android.content.IntentFilter
 import android.content.IntentSender
 import android.os.Build
 import android.telephony.TelephonyManager
 import android.util.Log
 import com.google.android.gms.auth.api.credentials.Credential
-import io.flutter.plugin.common.MethodCall
-import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
-import com.google.android.gms.auth.api.phone.SmsRetriever;
+import com.google.android.gms.auth.api.credentials.Credentials
+import com.google.android.gms.auth.api.credentials.HintRequest
+import com.google.android.gms.auth.api.phone.SmsRetriever
 import com.google.android.gms.auth.api.phone.SmsRetrieverClient
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry
-import com.google.android.gms.auth.api.credentials.Credentials
-import com.google.android.gms.auth.api.credentials.HintRequest
-import io.flutter.plugin.common.PluginRegistry.Registrar
-
 
 /** SmsOtpAutoVerifyPlugin */
-class SmsOtpAutoVerifyPlugin : FlutterPlugin, MethodCallHandler,
+class SmsOtpAutoVerifyPlugin : FlutterPlugin, MethodChannel.MethodCallHandler,
     MySmsListener, ActivityAware {
+
     private var channel: MethodChannel? = null
     private var pendingResult: MethodChannel.Result? = null
     private var receiver: SmsBroadcastReceiver? = null
@@ -37,43 +33,35 @@ class SmsOtpAutoVerifyPlugin : FlutterPlugin, MethodCallHandler,
     private var activity: Activity? = null
     private var binding: ActivityPluginBinding? = null
 
-    private val activityResultListener: PluginRegistry.ActivityResultListener =
-        object : PluginRegistry.ActivityResultListener {
-            override fun onActivityResult(
-                requestCode: Int,
-                resultCode: Int,
-                data: Intent?
-            ): Boolean {
-                if (requestCode == REQUEST_RESOLVE_HINT) {
-                    if (resultCode == Activity.RESULT_OK && data != null) {
-                        val credential: Credential? = data.getParcelableExtra(Credential.EXTRA_KEY)
-                        val phoneNumber: String? = credential?.id
-                        pendingResult?.success(phoneNumber)
-                    } else {
-                        pendingResult?.success(null)
-                    }
-                    return true
+    private val activityResultListener =
+        PluginRegistry.ActivityResultListener { requestCode, resultCode, data ->
+            if (requestCode == REQUEST_RESOLVE_HINT) {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    val credential: Credential? = data.getParcelableExtra(Credential.EXTRA_KEY)
+                    val phoneNumber: String? = credential?.id
+                    pendingResult?.success(phoneNumber)
+                } else {
+                    pendingResult?.success(null)
                 }
-                return false
+                return@ActivityResultListener true
             }
+            false
         }
 
     companion object {
-        private const val channelName = "sms_otp_auto_verify"
+        private const val CHANNEL_NAME = "sms_otp_auto_verify"
         private const val REQUEST_RESOLVE_HINT = 1256
 
         @JvmStatic
-        fun setup(plugin: SmsOtpAutoVerifyPlugin, binaryMessenger: BinaryMessenger) {
-            plugin.channel = MethodChannel(binaryMessenger, channelName)
+        fun setup(plugin: SmsOtpAutoVerifyPlugin, messenger: BinaryMessenger) {
+            plugin.channel = MethodChannel(messenger, CHANNEL_NAME)
             plugin.channel?.setMethodCallHandler(plugin)
             plugin.binding?.addActivityResultListener(plugin.activityResultListener)
-
         }
     }
 
-
     override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        setup(plugin = this, binding.binaryMessenger)
+        setup(this, binding.binaryMessenger)
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
@@ -84,26 +72,26 @@ class SmsOtpAutoVerifyPlugin : FlutterPlugin, MethodCallHandler,
         when (call.method) {
             "getAppSignature" -> {
                 activity?.let {
-                    val signature = AppSignatureHelper(it).getAppSignatures()[0]
+                    val signature = AppSignatureHelper(it).getAppSignatures().firstOrNull()
                     result.success(signature)
-                }
-
+                } ?: result.success(null)
             }
+
             "startListening" -> {
-                this.pendingResult = result
+                pendingResult = result
                 receiver = SmsBroadcastReceiver()
                 startListening()
-
             }
+
             "stopListening" -> {
                 pendingResult = null
                 unregister()
+                result.success(null)
             }
 
             "requestPhoneHint" -> {
-                this.pendingResult = result
+                pendingResult = result
                 requestHint()
-
             }
 
             else -> result.notImplemented()
@@ -120,10 +108,10 @@ class SmsOtpAutoVerifyPlugin : FlutterPlugin, MethodCallHandler,
             .setPhoneNumberIdentifierSupported(true)
             .build()
 
-        activity?.apply {
-            val intent = Credentials.getClient(this).getHintPickerIntent(hintRequest)
+        activity?.let {
+            val intent = Credentials.getClient(it).getHintPickerIntent(hintRequest)
             try {
-                startIntentSenderForResult(
+                it.startIntentSenderForResult(
                     intent.intentSender,
                     REQUEST_RESOLVE_HINT, null, 0, 0, 0
                 )
@@ -134,80 +122,65 @@ class SmsOtpAutoVerifyPlugin : FlutterPlugin, MethodCallHandler,
     }
 
     private fun isSimSupport(): Boolean {
-        val telephonyManager: TelephonyManager =
-            activity!!.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-        return (telephonyManager.simState == TelephonyManager.SIM_STATE_ABSENT)
+        val telephonyManager =
+            activity?.getSystemService(Context.TELEPHONY_SERVICE) as? TelephonyManager
+        return telephonyManager?.simState != TelephonyManager.SIM_STATE_ABSENT
     }
 
     private fun startListening() {
         activity?.let {
             client = SmsRetriever.getClient(it)
-        }
-        val task = client?.startSmsRetriever()
-        task?.addOnSuccessListener {
-            // Successfully started retriever, expect broadcast intent
-            unregister()
-            Log.e(javaClass::getSimpleName.name, "task started")
-            receiver?.setSmsListener(this)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                activity?.registerReceiver(receiver, IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION),
-                    Context.RECEIVER_EXPORTED)
+            val task = client?.startSmsRetriever()
+            task?.addOnSuccessListener {
+                unregister()
+                Log.d("SmsOtpAutoVerify", "SMS Retriever started")
+                receiver?.setSmsListener(this)
+                val filter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    it.registerReceiver(receiver, filter, Context.RECEIVER_EXPORTED)
+                } else {
+                    it.registerReceiver(receiver, filter)
+                }
             }
-            else {
-                activity?.registerReceiver(receiver, IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION))
-            }
-
         }
-
-
     }
 
     private fun unregister() {
         alreadyCalledSmsRetrieve = false
-        if (receiver!=null){
+        receiver?.let {
             try {
-                activity?.unregisterReceiver(receiver)
-                Log.d(javaClass::getSimpleName.name, "task stoped")
-                receiver = null
+                activity?.unregisterReceiver(it)
+                Log.d("SmsOtpAutoVerify", "SMS Retriever stopped")
             } catch (e: Exception) {
+                Log.e("SmsOtpAutoVerify", "Error unregistering receiver: ${e.message}")
+            } finally {
+                receiver = null
             }
         }
     }
 
     override fun onOtpReceived(message: String?) {
-        message?.let {
-            if (!alreadyCalledSmsRetrieve) {
-                pendingResult?.success(it)
-                alreadyCalledSmsRetrieve = true
-            }else{
-                Log.d( "onOtpReceived: ", "already called")
-            }
+        if (!alreadyCalledSmsRetrieve && message != null) {
+            pendingResult?.success(message)
+            alreadyCalledSmsRetrieve = true
         }
-
     }
 
-    override fun onOtpTimeout() {
-    }
-
+    override fun onOtpTimeout() {}
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
         activity = binding.activity
         this.binding = binding
-        binding.addActivityResultListener(activityResultListener); }
-
-    override fun onDetachedFromActivityForConfigChanges() {
-        unregister()
+        binding.addActivityResultListener(activityResultListener)
     }
+
+    override fun onDetachedFromActivityForConfigChanges() = unregister()
 
     override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
         activity = binding.activity
         this.binding = binding
-        binding.addActivityResultListener(activityResultListener);
+        binding.addActivityResultListener(activityResultListener)
     }
 
-    override fun onDetachedFromActivity() {
-        unregister()
-    }
-
-
+    override fun onDetachedFromActivity() = unregister()
 }
